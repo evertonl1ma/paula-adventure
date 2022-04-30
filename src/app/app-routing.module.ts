186f01d1f140d2d14c656fb3f696552b8aae50fb
Everton Lima <ever-tonlima@hotmail.com>
2022-04-29 21:30:50 -0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PresentationComponent } from './presentation/presentation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'presentation', component: PresentationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
